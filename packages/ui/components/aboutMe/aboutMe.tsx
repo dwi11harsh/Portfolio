00fc7459e529654befe612b8a682849c7541e725
Prabhat Dwivedi <harsh@Prabhats-MacBook-Air.local>
2024-02-19 11:07:35 +0530

    changed the strategy

--- packages/ui/components/aboutMe/aboutMe.tsx
+++ packages/ui/components/aboutMe/aboutMe.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 import { logo } from "../../public";
+import { TextGenerateEffect } from "../text-generate-effect";
 
 export const AboutMe = () => {
   return (
-    <section className="w-full py-6 md:py-12 lg:py-16">
+    <section className="flex items-cener justify-center w-full py-6 md:py-12 lg:py-16">
       <div className="container flex flex-col gap-6 px-4 md:gap-10 md:flex-row md:items-center lg:px-6 xl:gap-20">
         <div className="order-1 flex items-center justify-center w-full text-center md:order-2 md:w-1/2 md:text-left">
           <div className="space-y-3">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
-              Hi, I'm John Doe
-            </h1>
-            <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-              I'm a passionate and creative web developer with a love for clean
-              code and elegant user interfaces.
-            </p>
+            <div className="text-3xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+              <TextGenerateEffect words="Hi, I'm Prabhat Dwivedi" />
+            </div>
+            <div className="flex items-center justify-center mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+              <TextGenerateEffect words="I'm a passionate and creative web developer with a love for clean code and elegant user interfaces." />
+            </div>
           </div>
         </div>
         <div className="order-2 w-full md:order-1 md:w-1/2">
@@ -32,7 +32,7 @@ export const AboutMe = () => {
               />
             </div>
           </div>
-          <div className="md:hidden">
+          <div className="flex items-center justify-center md:hidden">
             <div className="relative overflow-hidden rounded-xl aspect-[2/1]">
               <Image
                 alt="Avatar"
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,5 @@
 export * from "./components/navbar";
 export * from "./components/aboutMe";
+export * from "./components/text-generate-effect";
+
+export * from "./utils/cn";
